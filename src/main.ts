ace46452c85ba8e7d5f40f8493abcd4a4f819590
mRutkowski99 <82534291+mRutkowski99@users.noreply.github.com>
2023-04-14 14:18:37 +0200

    signals-counter

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,73 @@
 import 'zone.js/dist/zone';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { signal } from './signals/signal';
 import { computed } from './signals/computed';
 import { effect } from './signals/effect';
 
+class HistoryLog {
+  readonly date: Date;
+
+  constructor(public readonly reservations: number) {
+    this.date = new Date();
+  }
+}
 
 @Component({
   selector: 'my-app',
   standalone: true,
   template: `
-    <div>Count: {{ count() }}</div>
-    <div>Double: {{ double() }}</div>
+    <p>Reserved tickets: {{ counter() }}</p>
+    <p>Will sales kick off: {{ willSalesKickOff() }}</p>
 
-    <button (click)="inc()">Increase</button>
+    <button [disabled]="isLimitReached()" (click)="increment()">Increase</button>
     <button (click)="reset()">Reset</button>
+    <button (click)="decrement()">Decrease</button>
 
     <br>
-    <!-- <test-arrays /> -->
-    <!-- <test-objects /> -->
+    
+    <button (click)="logHistory()">Log history</button>
 
   `,
 })
-export class App {
-  count = signal(0);
+export class App implements OnInit {
+  private readonly RESERVATIONS_MIN = 5;
+  private readonly RESERVATIONS_MAX = 20;
 
-  double = computed(() => this.count() * 2);
+  readonly counter = signal(0);
+  readonly willSalesKickOff = computed(
+    () => this.counter() >= this.RESERVATIONS_MIN
+  );
+  readonly isLimitReached = computed(
+    () => this.counter() === this.RESERVATIONS_MAX
+  );
 
-  countType = computed(() => (this.count() % 2 === 0 ? 'even' : 'odd'));
+  readonly history = signal<HistoryLog[]>([]);
 
-  constructor() {
+  ngOnInit() {
     effect(() => {
-      console.log('Count changed', this.count());
-      console.log(this.count(), 'is', this.countType());
+      this.history.mutate((historyLogs) =>
+        historyLogs.push(new HistoryLog(this.counter()))
+      );
     });
   }
 
-  inc() {
-    this.count.update((c) => c + 1);
+  increment() {
+    this.counter.update((counter) => counter + 1);
+  }
+
+  decrement() {
+    this.counter.update((counter) => (counter === 0 ? 0 : counter - 1));
   }
 
   reset() {
-    this.count.set(0);
+    this.counter.set(0);
+    this.history.set([]);
+  }
+
+  logHistory() {
+    console.log(this.history());
   }
 }
 
